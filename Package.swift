289540a77eb68db5ce96391538e4dc51c5c3dd1f
jrappeneker <joshua@magisys.net>
2019-09-04 18:07:03 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Silica",
     dependencies: [
-        .Package(url: "https://github.com/PureSwift/Cairo.git", majorVersion: 1)
+        .package(url: "https://github.com/PureSwift/Cairo.git", majorVersion: 1)
     ],
     targets: [
         Target(name: "Silica")
