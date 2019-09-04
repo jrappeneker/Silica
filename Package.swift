94df8b52cc76948ff2f989ecbd86b1f861f47a3c
jrappeneker <joshua@magisys.net>
2019-09-04 18:30:06 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,24 @@
 // swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "Silica",
+    products: [
+        .library(
+            name: "Silica",
+            targets: ["Silica"]),
+        ],
     dependencies: [
-        .package(url: "https://github.com/PureSwift/Cairo.git", from: "1.0.0")
+        .package(url: "https://github.com/serhii-londar/Cairo.git", .branch("master")),
+        .package(url: "https://github.com/PureSwift/LittleCMS.git", .branch("master"))
     ],
     targets: [
-        .target(name: "Silica")
-    ]
+        .target(
+            name: "Silica",
+            dependencies: ["Cairo", "LittleCMS"]),
+        .testTarget(
+            name: "SilicaTests",
+            dependencies: ["Silica"]),
+        ]
 )
